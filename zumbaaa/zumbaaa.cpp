452a45168cdd90abfafa781a0905bc1fa3f89759
Chirila Catalin <chirilacatalin93@gmail.com>
2022-02-16 22:31:07 +0200

    In acest moment programul stie lungimea sirului pe care trebuie sa il elimine

--- zumbaaa/zumbaaa.cpp
+++ zumbaaa/zumbaaa.cpp
@@ -2,6 +2,33 @@
 #include <iostream>
 #include<vector>
 
+int CelMaiMareSir(std::vector <int> vect) {
+	//cand se va apela functia se va introduce ca parametru vectorul de frecvente
+	int max = 0;
+	for (int i = 1; i < vect.size() - 1; i++)
+	{
+		if (vect[max] < vect[i])
+			max = i;
+	}
+	return max;
+}
+int LungimeSirEliminat(int iMax_m, std::vector <int> frecv_m, std::vector <int> cifra_m)
+{
+	int n = 0;
+	do {
+		if (cifra_m[iMax_m - n - 1] == cifra_m[iMax_m + n + 1])
+		{
+			if ((frecv_m[iMax_m - n - 1] + frecv_m[iMax_m + n + 1]) >=3)
+				n++;
+			else
+				return n;
+		}
+		else
+			return n;
+	} while (n < 20); //todo: while ((n < frecv_m.size()) / 2);
+	return n;
+
+}
 
 int main()
 {
@@ -55,8 +82,13 @@ int main()
 	{
 		std::cout << it << "-";
 	}
-
-
+	int iMax;
+	iMax = CelMaiMareSir(frecv);
+	std::cout << std::endl;
+	std::cout << "Cel mai mare sir se afla pe pozitia "<<iMax << std::endl;
+	int n = 0;
+	n=LungimeSirEliminat(iMax, frecv, cifra);
+	std::cout << "Latimea sirului ce trebuie eliminat este " << n << std::endl;
 
 
 }
